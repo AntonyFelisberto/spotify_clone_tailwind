2a0980cc2f3550d93a1850dd44f77f376c37e26e
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2024-03-30 18:21:40 -0300

    banner principal

--- projeto/tailwind.config.js
+++ projeto/tailwind.config.js
@@ -1,31 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
       spacing:{
-        "14":"3.5rem",
-        "125px":"125px"
+        '14': '3.5rem',
+        '125px': '125px'
       },
       colors: {
-        "hoverspt":"#18D760",
-        "purple-main":"#1ED760",
-        "green-main":"1ED760"
+        'hoverspt': '#18D760',
+        'purple-main': '#2D46B9',
+        'green-main': '#1ED760'
       },
-      backgroundImage: theme => ({
-        "spotify-theme": "url('/img/bursts.svg')",
-        "spotify-theme-mobile": "url('/img/bursts-mobile.svg')",
-      }),
       backgroundSize: {
-        "175%":"175%",
-        "195%":"195%"
+        '175%': '175%',
+        '195%': '195%'
       },
       backgroundPosition: {
-        "banner":"46% 4%",
-        "banner-mobile":"top 25% center"
+        'banner': '46% 4%',
+        'banner-mobile': 'top 25% center'
       },
       fontSize: {
-        "9xl":"9rem"
+        '9xl': '9rem'
       }
     },
   },
@@ -33,3 +29,7 @@ module.exports = {
   plugins: [],
 }
 
+
+
+
+   
